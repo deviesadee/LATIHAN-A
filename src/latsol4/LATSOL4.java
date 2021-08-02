15eb45300795d20c7f8e1e69544c2d5eb6752c37
2nd <2nd@DESKTOP-UQ980KK>
2021-08-02 18:55:25 +0700

    HAII

---
+++ src/latsol4/LATSOL4.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latsol4;
+
+/**
+ *
+ * @author 2nd
+ */
+public class LATSOL4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int awal = 5;
+       int beda =5 ;
+        int akhir = ;
+        
+        for ( int a =0; a <=4;a++){
+        for (int  b =1;  b<= a; b++){
+            
+            akhir = awal;
+            awal += beda;
+            System.out.print(" "+akhir);
+        }
+       System.out.println(" ");
+        }
+
+       
+    }
+    
+}
